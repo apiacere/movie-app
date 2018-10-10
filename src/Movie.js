5436fd0e70f75ffd571a02c9c5914cc539044095
kangjae jang <apiacere05+gitlab@gmail.com>
2018-10-10 11:40:26 +0900

    Edit stateless class component to function component

--- src/Movie.js
+++ src/Movie.js
@@ -1,35 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component {
-
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    }
-
-    render () {
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster}></MoviePoster>
-                <h1>{this.props.title}</h1>
-            </div>
-        )
-    }
+function Movie ({title, poster}) {
+    return (
+        <div>
+            <MoviePoster poster={poster}></MoviePoster>
+            <h1>{title}</h1>
+        </div>
+    )
 }
 
-class MoviePoster extends Component {
+function MoviePoster ({poster}) {
+    return <img src={poster} alt=""></img>
+}
 
-    static propTypes = {
-        poster: PropTypes.string.isRequired
-    }
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+}
 
-    render() {
-        return (
-            <img src={this.props.poster} alt=""></img>
-        )
-    }
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
 }
 
 export default Movie;
